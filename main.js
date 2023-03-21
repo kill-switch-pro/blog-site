fe090dc1f18f54a567d5f49129a467fc9be9c5fb
psalm <stephensamuel063@gmail.com>
2023-03-21 14:33:09 -0800

    aling well

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-let btn = document.getElementById("accordion");
-btn.addEventListener("click", function () {
-  let panel = document.getElementById("panel");
+let btnShow = document.getElementById("open-menu");
+let btnmenu = document.getElementById("close-menu");
+let welcome = document.getElementById("welcome-message");
+btnShow.addEventListener("click", function () {
+  let panel = document.getElementById("menu-panel");
   panel.style.transition = "height 2s";
-  panel.style.height = "10rem";
+  panel.style.height = "15rem";
+  btnShow.style.display = "none";
 });
-
-btn.addEventListener("focusout", function () {
-  let panel = document.getElementById("panel");
+/*
+window.addEventListener("load", function () {
+  welcome.style.transition = "width 3s";
+  welcome.style.width = "60rem";
+});
+*/
+btnmenu.addEventListener("click", function () {
+  let panel = document.getElementById("menu-panel");
   panel.style.transition = "height 0.6s";
   panel.style.height = 0;
+  btnShow.style.display = "block";
 });
+
 //question , why cant classname work for dom
